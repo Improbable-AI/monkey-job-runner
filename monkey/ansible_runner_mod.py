7a58ed07f4f58503d1d343e0939481ebedae43ed
Avery Lamp <averylamp@gmail.com>
2020-04-15 05:36:57 -0400

    Interfaces with ansible through ansible runner

---
+++ monkey/ansible_runner_mod.py
@@ -0,0 +1,10 @@
+import ansible_runner
+
+r = ansible_runner.run(playbook='create_job.yml', private_data_dir='project')
+
+print(r)
+for each_host_event in r.events:
+    print(each_host_event['event'])
+
+
+print(r.stats)
