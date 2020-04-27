94549c12ca874973ad03d350faa2dcfa09865f5b
Avery Lamp <averylamp@gmail.com>
2020-04-27 17:21:35 -0400

    Moves variable overwriting to conditional block

--- monkey/ansible_runner_mod.py
+++ monkey/ansible_runner_mod.py
@@ -10,7 +10,7 @@ import ansible_runner
 # print(r.stats)
 
 
-r = ansible_runner.run(playbook='gcp_create_job.yml', private_data_dir='ansible')
+r = ansible_runner.run(playbook='gcp_create_job.yml', private_data_dir='ansible', inventory="ansible/inventory")
 
 print(r)
 for each_host_event in r.events:
