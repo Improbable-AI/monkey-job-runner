f9ab7222262fe65061bcd28af40f0132890e0366
Avery Lamp <averylamp@gmail.com>
2020-04-27 17:04:09 -0400

    Modifies project structure and inventory to use group vars.  writes locally gcp group vars to ensure sshkey and user are able to be restored for future use

--- monkey/ansible_runner_mod.py
+++ monkey/ansible_runner_mod.py
@@ -1,6 +1,16 @@
 import ansible_runner
 
-r = ansible_runner.run(playbook='create_job.yml', private_data_dir='ansible')
+# r = ansible_runner.run(host_pattern='all', private_data_dir='ansible', cmdline="--list-hosts")
+
+# print(r)
+# for each_host_event in r.events:
+#     print(each_host_event['event'])
+
+
+# print(r.stats)
+
+
+r = ansible_runner.run(playbook='gcp_create_job.yml', private_data_dir='ansible')
 
 print(r)
 for each_host_event in r.events:
