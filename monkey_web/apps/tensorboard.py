79cbda98674cdca69737ba3e03159dc65218ab72
Jerry Mao <j-mao@users.noreply.github.com>
2021-07-09 21:13:44 +1000

    Add tensorboard

--- monkey_web/apps/tensorboard.py
+++ monkey_web/apps/tensorboard.py
@@ -4,8 +4,37 @@ from dash.dependencies import Input, Output, State
 
 import json
 import requests
+from tensorboard import program
+import threading
 
 from app import app, MONKEY_CORE, MONKEY_STATUS, get_run_list
+
+
+tensorboard_registry = {}
+lock = threading.Lock()
+
+class TensorboardViewer:
+    def __init__(self, project_id, run_id, logdir, interval=3600.0):
+        self.project_id = project_id
+        self.run_id = run_id
+        tb = program.TensorBoard()
+        tb.configure(argv=[None, '--logdir', logdir])
+        self.server = tb._make_server()
+        self.thread = threading.Thread(target=self.server.serve_forever, name="TensorBoard")
+        self.thread.daemon = True
+        self.manager = threading.Timer(interval, self.shutdown)
+        self.manager.daemon = True
+
+    def launch(self):
+        self.thread.start()
+        self.manager.start()
+        self.url = self.server.get_url()
+        tensorboard_registry[self.project_id, self.run_id] = self
+
+    def shutdown(self):
+        with lock:
+            del tensorboard_registry[self.project_id, self.run_id]
+        self.server.shutdown()
 
 
 def get_layout(project_id, run_id):
@@ -25,5 +54,24 @@ def get_layout(project_id, run_id):
         ])
 
 
+def get_run_monkeyfs_path(project_id, run_id):
+    try:
+        r = requests.get(f'{MONKEY_CORE}/get/job_info', params={'job_uid': run_id})
+        r.raise_for_status()
+        return r.json()['job_info']['provider_vars']['local_monkeyfs_path']
+    except:
+        raise
+
 def get_tensorboard_link(project_id, run_id):
-    return 'http://localhost:6006'
+    print('Generating tensorboard link')
+    with lock:
+        if (project_id, run_id) in tensorboard_registry:
+            print('Returning pre-existing link')
+            return tensorboard_registry[project_id, run_id].url
+        print('Creating tensorboard object')
+        logdir = get_run_monkeyfs_path(project_id, run_id)
+        viewer = TensorboardViewer(project_id, run_id, logdir=logdir)
+        print('Launching tensorboard')
+        viewer.launch()
+    print('Returning link', viewer.url)
+    return viewer.url
